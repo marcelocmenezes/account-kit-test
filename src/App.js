27f2d739d41cfeb1324e9912f8e990580dc1de6b
Marcelo Menezes <marcelomenezes.0907@gmail.com>
2019-02-22 16:27:36 -0300

    fix: Logged

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import AccountKit from 'react-facebook-account-kit';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = { 
+      isLogged: false
+    }
+  }
   render() {
     return (
       <div className="App">
+      { 
+        !this.state.isLogged ?
         <AccountKit
           appId="897877397220204" // Update this!
           version="v1.1" // Version must be in form v{major}.{minor}
-          onResponse={(resp) => console.log(resp)}
-          csrf={'csrf token here!'} // Required for security
+          onResponse={(resp) => {if(resp.status === 'PARTIALLY_AUTHENTICATED') this.setState({isLogged: true})}}
+          csrf={'Marcelo'} // Required for security
           countryCode={'+55'} // eg. +60
           phoneNumber={'11944497711'} 
           language={'pt_BR'}
-          
         >
           {p => <button {...p}>Initialize Account Kit</button>}
         </AccountKit>
+        : <h4>Logado!</h4>
+        }
       </div>
     );
   }
